8170c0e7bdd1d3429a3a21872b485d26e6b94f97
JJJamieee <b07705033@ntu.edu.tw>
2021-01-21 18:09:28 +0800

    edit routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import routes from './routes'
+import routes from './routes/cups'
 import mongoose from 'mongoose'
 import cupsRoute from './routes/cups'
 
